676354fc95ce59b192c368736043757f2de36316
mercydaniels <mercy.daniels@blackburn.edu>
2020-02-20 13:44:49 -0600

    setup done

---
+++ src/emptyjavaproject/genericHeroClass.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emptyjavaproject;
+
+import com.pauliankline.floopyconnector.BaseHero;
+
+/**
+ *
+ * @author mercy.daniels
+ */
+public class genericHeroClass extends BaseHero{
+
+    @Override
+    public boolean isInBattle() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String enemy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void gameTickAction(long arg0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void die() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isDead() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
